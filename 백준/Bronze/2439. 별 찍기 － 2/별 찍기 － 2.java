c677e07fd8c861b586b832654a1f2788e0b5d41e
2taeyoung <89830089+Taeyoung92@users.noreply.github.com>
2025-01-16 23:31:09 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=0; j<n-i; j++) {
+               bw.write(" "); 
+            }
+            for(int k=0; k<i; k++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
