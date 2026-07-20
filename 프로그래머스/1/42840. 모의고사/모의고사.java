810763000f1c05e5f008774d8bac45e19f35df4f
2taeyoung <89830089+Taeyoung92@users.noreply.github.com>
2026-07-20 15:12:48 +0900

    [level 1] Title: 모의고사, Time: 0.54 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] count = {0,0,0};
+        
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == first[i%5]) count[0]++;
+            if(answers[i] == second[i%8]) count[1]++;
+            if(answers[i] == third[i%10]) count[2]++;
+        }
+        
+        int max = Math.max(count[0], Math.max(count[1], count[2]));
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<3; i++) {
+            if(count[i] == max) {
+                list.add(i + 1);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
